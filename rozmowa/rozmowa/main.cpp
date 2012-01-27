135edf63545793a62d0dcedfc9140d357c9d81f9
Marcin Michalik s8132 <s8132@pjwstk.edu.pl>
2012-01-27 03:56:12 +0100

    finish

--- rozmowa/rozmowa/main.cpp
+++ rozmowa/rozmowa/main.cpp
@@ -11,18 +11,61 @@ using namespace std;
 
 int main(int argc, char* args[]){
 
-	string jeden;
+	string neutralnyText, negatywnyText, pozytywnyText;
 	bool done=true;
+	int nastawienie = 1, odpowiedz;
+	/*
+		NASTAWIENIE:
+		0 - negtywne
+		1 - neutralne
+		2 - pozytywne
+	*/
 
-	fstream pierwszy( "start.txt", std::ios::in );
+	fstream neutralny( "neutralny.txt", std::ios::in );
+	fstream negatywny( "negatywny.txt", std::ios::in );
+	fstream pozytywny( "pozytywny.txt", std::ios::in );
 
 	while(done){
 		for(int i=0; i<4; i++){
-			getline(pierwszy, jeden);
-			cout << jeden << endl;
+			if(nastawienie==0){
+				getline(negatywny, negatywnyText);
+				if(negatywnyText=="KONIEC"){
+					done = false;
+					break;
+				}
+				cout << negatywnyText << endl;
+			}
+			if(nastawienie==1){
+				getline(neutralny, neutralnyText);
+				if(neutralnyText=="KONIEC"){
+					done = false;
+					break;
+				}
+				cout << neutralnyText << endl;
+			}
+			if(nastawienie==2){
+				getline(pozytywny, pozytywnyText);
+				if(pozytywnyText=="KONIEC"){
+					done = false;
+					break;
+				}
+				cout << pozytywnyText << endl;
+			}
+		}
+		if(done==false){
+			break;
 		}
 
+		cin >> odpowiedz;
 
+		if(odpowiedz==1){
+			if(nastawienie<2) nastawienie++;
+			else nastawienie=2;
+		}
+		if(odpowiedz==2){
+			if(nastawienie>0) nastawienie=0;
+			else nastawienie=0;
+		}
 	}
 	getch();
 
